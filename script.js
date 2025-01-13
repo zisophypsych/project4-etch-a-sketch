1121c46bfe2192c258befa5656bb0865d038d46b
Ziyaul Haq <ziyaulhaqkhan60@gmail.com>
2025-01-13 15:16:36 +0530

    Pixelated Etch a sketch!
    Used Event listenors, Loops , added some buttons, flex-box

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const body = document.querySelector('body');
+const gameScreen = document.querySelector('#video-game');
 const container = document.querySelector('.container');
 const gridButton = document.querySelector('#grid-btn');
+const clearButton = document.querySelector('#clear');
+const eraser = document.querySelector('#eraser');
+const randomColor = document.querySelector('#random-colors');
+
+note = document.createElement('div')
+note.style.backgroundColor = 'lightpink'
+body.appendChild(note);
 
 let gridSize = 16;
 
-function resizeGrid() {
 
-    // const container = document.createElement('div');
-    // container.setAttribute('class', 'container');
-    // body.appendChild(container);
-    
+function resizeGrid() {
 
     for(let i = 0; i < gridSize; i++) {
 
@@ -27,7 +31,17 @@ function resizeGrid() {
 
 }
 
+function squareBackColor(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+        });
+    });
+}
+
 resizeGrid();
+squareBackColor();
 
 
 gridButton.addEventListener('click', () => {
@@ -37,6 +51,7 @@ gridButton.addEventListener('click', () => {
         gridSize = gridNumber;
         container.innerHTML = '';
         resizeGrid()
+        squareBackColor();
     } else {
         alert("Invalid Grid Size!!")
     }
@@ -44,5 +59,31 @@ gridButton.addEventListener('click', () => {
 
 })
 
+clearButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+    })
+})
 
+eraser.addEventListener('click',squareBackColor = () => {
+    eraser.style.backgroundColor = 'yellow';
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'white';
+        });
+    });
+    note.innerText = 'Note: Double click on eraser button to disable it.'
+} )
 
+eraser.addEventListener('dblclick',squareBackColor = () => {
+    eraser.style.backgroundColor = 'white'
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+        });
+    });
+    note.innerText = ''
+} )
